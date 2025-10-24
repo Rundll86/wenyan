e42f845424a0719993efd4a17fac57dad7744274
陨落基围虾 <3161880837@qq.com>
2025-10-24 17:58:31 +0800

    refactor(runtime): 重构内置模块加载方式并更新测试文档
    将静态导出改为动态加载模块，以支持运行时自动发现和加载内置模块
    添加@types/webpack-env依赖以支持动态加载类型
    更新测试文档中的循环逻辑和条件判断

--- src/engine/runtime/builtins/lib.ts
+++ src/engine/runtime/builtins/lib.ts
@@ -1,4 +1,11 @@
-export * as 志者 from "./志者";
-export * as 天命也 from "./天命也";
-export * as 春秋 from "./春秋";
-export * as 四书五经 from "./四书五经";
+// export * as 志者 from "./志者";
+// export * as 天命也 from "./天命也";
+// export * as 春秋 from "./春秋";
+// export * as 四书五经 from "./四书五经";
+const load = require.context("./", true, /\.ts$/);
+load.keys().forEach((key) => {
+    const module = load(key);
+    if (module.default) {
+        Object.assign(exports, module.default);
+    }
+});
